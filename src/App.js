aa53f372753a947491421353c2292be4ecc5bb6b
saidugurlu <ugurlumsaid@gmx.de>
2022-05-23 16:50:30 +0200

    static list added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@
 
 function App() {
   return (
-    <div className="mainWrapper">
+    <div classNameName="mainContainer">
  <header>
 
  </header>
- <section className="postList">
-
+ <section classNameName="postList">
+ <div className="ui relaxed divided list">
+  <div className="item">
+    <i className="large github middle aligned icon"></i>
+    <div className="content">
+      <a className="header">Semantic-Org/Semantic-UI</a>
+      <div className="description">Updated 10 mins ago</div>
+    </div>
+  </div>
+  <div className="item">
+    <i className="large github middle aligned icon"></i>
+    <div className="content">
+      <a className="header">Semantic-Org/Semantic-UI-Docs</a>
+      <div className="description">Updated 22 mins ago</div>
+    </div>
+  </div>
+  <div className="item">
+    <i className="large github middle aligned icon"></i>
+    <div className="content">
+      <a className="header">Semantic-Org/Semantic-UI-Meteor</a>
+      <div className="description">Updated 34 mins ago</div>
+    </div>
+  </div>
+</div>
  </section>
     </div>
   );
