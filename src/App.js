d725a1ebf2066c62bbc5a0a43cbc10dda9542c3b
saidugurlu <ugurlumsaid@gmx.de>
2022-05-23 19:48:59 +0200

    list componenet cleaned

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
-
-
 function App() {
   return (
     <div classNameName="mainContainer">
- <header>
+      <header></header>
 
- </header>
- <section classNameName="postList">
- <div className="ui relaxed divided list">
-  <div className="item">
-    <i className="large github middle aligned icon"></i>
-    <div className="content">
-      <a className="header">Semantic-Org/Semantic-UI</a>
-      <div className="description">Updated 10 mins ago</div>
-    </div>
-  </div>
-  <div className="item">
-    <i className="large github middle aligned icon"></i>
-    <div className="content">
-      <a className="header">Semantic-Org/Semantic-UI-Docs</a>
-      <div className="description">Updated 22 mins ago</div>
-    </div>
-  </div>
-  <div className="item">
-    <i className="large github middle aligned icon"></i>
-    <div className="content">
-      <a className="header">Semantic-Org/Semantic-UI-Meteor</a>
-      <div className="description">Updated 34 mins ago</div>
-    </div>
-  </div>
-</div>
- </section>
+      <section classNameName="postList">
+        <div className="ui raised very padded text container segment">
+          <div className="item">
+            <i className="large github middle aligned icon"></i>
+            <div className="content">
+              <a href="#www" className="header">
+                Semantic-Org/Semantic-UI
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
