4ab5b9b1147941b8170cdf8646451dd32d1df515
saidugurlu <ugurlumsaid@gmx.de>
2022-05-26 13:07:11 +0200

    artikellist componenet work

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+
 import Artikelliste from './components/Artikelliste'
 
 function App() {
-  const [articles, setArticles] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      const response = await axios.get(
-        "https://react-yazi-yorum.herokuapp.com/posts"
-      );
-   
-      setArticles(response.data); 
-
-
-    })();
-  }, []);
-  console.log(articles);
+  
   return (
     <div className="mainContainer">
       <header></header>
 
       <div className="ui raised very padded text container segment">
         <div className="ui relaxed divided list">
-          {articles.map((article, index) => {
-            return (
-              <div key={index} className="item">
-                <i className="large github middle aligned icon"></i>
-                <div className="content">
-                  <a href="#d" className="header">
-                    {article.title}
-                  </a>
-                  <div className="description">{article.created_at}</div>
-                </div>
-              </div>
-            );
-          })}
+       <Artikelliste/>
         </div>
       </div>
     </div>
--- src/components/Artikelliste.js
+++ src/components/Artikelliste.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Artikelliste = () => {
-  return (
-    <div>Artikelliste</div>
-  )
-}
+    const [articles, setArticles] = useState([]);
 
-export default Artikelliste
+    useEffect(() => {
+      (async () => {
+        const response = await axios.get(
+          "https://react-yazi-yorum.herokuapp.com/posts"
+        );
+     
+        setArticles(response.data); 
+  
+  
+      })();
+    }, []);
+  return articles.map((article, index) => {
+    return (
+      <div key={index} className="item">
+        <i className="large github middle aligned icon"></i>
+        <div className="content">
+          <a href="#d" className="header">
+            {article.title}
+          </a>
+          <div className="description">{article.created_at}</div>
+        </div>
+      </div>
+    );
+  });
+};
+
+export default Artikelliste;
