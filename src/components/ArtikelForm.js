a98ae2578b16462bdf409f879e9f6df89fc41029
saidugurlu <ugurlumsaid@gmx.de>
2022-05-29 16:15:09 +0200

    error message work

--- src/components/ArtikelForm.js
+++ src/components/ArtikelForm.js
@@ -1,24 +1,37 @@
-import { useState} from "react";
+import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const ArtikelForm = () => {
   const [article, setArticle] = useState({ title: "", content: "" });
   const navigate = useNavigate();
+  const [error, setError] = useState("");
 
   const onInputChange = (e) =>
     setArticle({ ...article, [e.target.name]: e.target.value });
 
   const onFormSubmit = async () => {
-    await axios.post("https://react-yazi-yorum.herokuapp.com/posts", article);
-    setArticle(article);
-    navigate("/")
+    setError("")
+   try {await axios.post("https://react-yazi-yorum.herokuapp.com/posts", article);
+    navigate("/", {replace: true});
+  } catch (error){
+    setError("Artikeltitel und Textinhalt müssen ausgefüllt werden!")
+  }
   };
 
-
-
   return (
-    <div className="ui form">
+
+    <div className="ui form">{
+      error && (  <div className="ui negative message">
+ 
+      <div className="header">
+        Ups!
+      </div>
+      <p>{error}
+    </p></div>)
+    }
+  
+
       <div className="field">
         <label>Artikeltitel</label>
         <input
